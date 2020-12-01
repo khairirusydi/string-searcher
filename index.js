97a5c03d66c8c47f7c49a86b9cb5b807e1deca78
Khairi Rusydi <krusydi@palo-it.com>
2020-12-01 15:50:40 +0800

    Created function getAllFiles to get all files in current working directory

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
 #!/usr/bin/env node
+var fs = require('fs');
 
-console.log('current directory:', process.cwd());
-console.log('keyword:', process.argv[2] || 'TODO');
+// recursively get all files in directory
+const getAllFiles = (dir, currentArrayOfFiles) => {
+  const newArrayOfFiles = currentArrayOfFiles || [];
+  const dirItems = fs.readdirSync(dir);
+  dirItems.forEach(item => {
+    const itemName = dir + '/' + item;
+    if (fs.statSync(itemName).isDirectory()) {
+      getAllFiles(itemName, newArrayOfFiles);
+    } else {
+      newArrayOfFiles.push(itemName);
+    };
+  });
+  return newArrayOfFiles;
+};
+
+const mainApp = () => {
+  const dir = process.cwd();
+  // const searchString = process.argv[2] || 'TODO';
+
+  const files = getAllFiles(dir);
+
+  return files;
+};
+
+console.log(mainApp().join('\r\n'));
